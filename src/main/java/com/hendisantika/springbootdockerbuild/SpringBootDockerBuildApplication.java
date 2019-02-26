58de69876286b6a87f510db7e13a9da5fb709afd
hendisantika <hendisantika@yahoo.co.id>
2019-02-27 05:40:06 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootdockerbuild/SpringBootDockerBuildApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdockerbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerBuildApplication.class, args);
+    }
+
+}
